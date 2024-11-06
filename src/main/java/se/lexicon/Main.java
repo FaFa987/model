7cf85a9e84a07e226775ceb0d20b2130f873413b
Fatemeh <f.khalleghi.777@gmail.com>
2024-11-06 10:59:33 +0100

    product1

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -4,6 +4,10 @@ package se.lexicon;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Product p1 = new Product(1,2.5, "popcorn");
+
+        System.out.println(p1.productInfo());
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
--- src/main/java/se/lexicon/Product.java
+++ src/main/java/se/lexicon/Product.java
@@ -4,4 +4,28 @@ public class Product {
     private int id;
     private double price;
     private String ptoductName;
+    public Product(int id, double price, String ptoductName) {
+        this.id = id;
+        this.price = price;
+        this.ptoductName = ptoductName;
+    }
+    public int getId() {
+        return id;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getPtoductName() {
+        return ptoductName;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String productInfo(){
+        return "you chose: " + ptoductName + ", price: " + price;
+    }
+
 }
